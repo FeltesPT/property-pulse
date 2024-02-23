7b738f2b82102047a475879982002916f8c407e4
Tiago Dias <feltes.pt@gmail.com>
2024-02-23 16:33:56 +0000

    Section 1 complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,24 @@
 import React, { ReactNode } from 'react';
 import '@/assets/styles/global.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'Property Pulse | Find The Perfect Rental',
   description: 'Find the perfect rental in your area',
-  keywords: 'Property Pulse, Rental, Real Estate, Property, Property Search, Property Finder, Property Listings',
-}
+  keywords:
+    'Property Pulse, Rental, Real Estate, Property, Property Search, Property Finder, Property Listings',
+};
 
-type LayoutProps = {children?: ReactNode}
+type LayoutProps = { children?: ReactNode };
 
 const MainLayout: React.FC = ({ children }: LayoutProps) => {
   return (
     <html lang='en'>
       <body>
+        <Navbar />
         <div>{children}</div>
+        <Footer />
       </body>
     </html>
   );
